dcf26d25127315f5069ac96cb0953e6ac7476b7f
Jonathan Peris <josilva07@grupoamil.com.br>
2018-03-01 15:13:13 -0300

    Refatoração na camada de apresentação do sistema

--- Configuration/ApplicationConfigurations.cs
+++ Configuration/ApplicationConfigurations.cs
@@ -3,7 +3,5 @@ namespace dotnet_cpnucleo_pages.Configuration
     public class ApplicationConfigurations 
     {
         public string NomeSistema { get; set; }
-
-        public string Copyright { get; set; }
     }    
 }
