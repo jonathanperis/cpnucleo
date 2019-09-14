15954402b5779a4b21aeb929dd3eff21af3c8d30
Jonathan Peris <jperis.silva@outlook.com>
2019-09-14 16:43:24 -0300

    [CHORE] Correção nas rotas de erro

--- src/Cpnucleo.RazorPages/Startup.cs
+++ src/Cpnucleo.RazorPages/Startup.cs
@@ -68,15 +68,13 @@ namespace Cpnucleo.RazorPages
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDeveloperExceptionPage();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                //app.UseExceptionHandler("/Erro");
+                app.UseExceptionHandler("/Erro");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
