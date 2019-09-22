3bda048e06ce6e443d7be4a0336f6c6805fa9dcd
Jonathan Peris <jperis.silva@outlook.com>
2019-09-21 21:57:41 -0300

    [FEAT] Criação do GRPC Client

---
+++ src/Cpnucleo.GRPC.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cpnucleo.GRPC.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
