65f3fc7f2336c7e7eb7a5a9445043a53f677682d
Jonathan Peris <jperis.silva@outlook.com>
2019-10-06 13:34:06 -0300

    [FEAT] Inclusão de novos projetos de teste

---
+++ src/Cpnucleo.Application.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cpnucleo.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Cpnucleo.Infra.Data.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cpnucleo.Infra.Data.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
