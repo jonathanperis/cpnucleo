c44be6ed416f21a87bd92a1c954f143df51c5d96
Jonathan Peris <jperis.silva@outlook.com>
2019-10-20 12:19:57 -0300

    [CHORE] Estruturação inicial do módulo de sistema

---
+++ src/Cpnucleo.RazorPages.Material/Pages/Sistema/Remover.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Cpnucleo.Application.Interfaces;
+using Cpnucleo.Infra.CrossCutting.Util.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Cpnucleo.RazorPages.Material.Pages.Sistema
+{
+    [Authorize]
+    public class RemoverModel : PageModel
+    {
+        private readonly ISistemaAppService _sistemaAppService;
+
+        public RemoverModel(ISistemaAppService sistemaAppService)
+        {
+            _sistemaAppService = sistemaAppService;
+        }
+
+        [BindProperty]
+        public SistemaViewModel Sistema { get; set; }
+
+        public IActionResult OnGet(Guid id)
+        {
+            Sistema = _sistemaAppService.Consultar(id);
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            _sistemaAppService.Remover(Sistema.Id);
+
+            return RedirectToPage("Listar");
+        }
+    }
+}
