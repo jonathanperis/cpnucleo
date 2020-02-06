5b387b1a33849561c17b0d619a42c2a2d7a16a8e
Jonathan Peris <jperis.silva@outlook.com>
2020-02-05 23:13:33 -0300

    [FEAT] Remoção de injeções de dependência desnecessárias

--- src/Cpnucleo.Infra.CrossCutting.IoC/ServiceCollectionExtension.cs
+++ src/Cpnucleo.Infra.CrossCutting.IoC/ServiceCollectionExtension.cs
@@ -29,14 +29,7 @@ namespace Cpnucleo.Infra.CrossCutting.IoC
             services
                 .AddScoped<ICrudAppService<SistemaViewModel>, CrudAppService<Sistema, SistemaViewModel>>()
                 .AddScoped<ICrudAppService<ProjetoViewModel>, CrudAppService<Projeto, ProjetoViewModel>>()
-                .AddScoped<ICrudAppService<TarefaViewModel>, CrudAppService<Tarefa, TarefaViewModel>>()
-                .AddScoped<ICrudAppService<ApontamentoViewModel>, CrudAppService<Apontamento, ApontamentoViewModel>>()
-                .AddScoped<ICrudAppService<WorkflowViewModel>, CrudAppService<Workflow, WorkflowViewModel>>()
-                .AddScoped<ICrudAppService<RecursoViewModel>, CrudAppService<Recurso, RecursoViewModel>>()
                 .AddScoped<ICrudAppService<ImpedimentoViewModel>, CrudAppService<Impedimento, ImpedimentoViewModel>>()
-                .AddScoped<ICrudAppService<ImpedimentoTarefaViewModel>, CrudAppService<ImpedimentoTarefa, ImpedimentoTarefaViewModel>>()
-                .AddScoped<ICrudAppService<RecursoProjetoViewModel>, CrudAppService<RecursoProjeto, RecursoProjetoViewModel>>()
-                .AddScoped<ICrudAppService<RecursoTarefaViewModel>, CrudAppService<RecursoTarefa, RecursoTarefaViewModel>>()
                 .AddScoped<ICrudAppService<TipoTarefaViewModel>, CrudAppService<TipoTarefa, TipoTarefaViewModel>>();
 
             services
@@ -55,13 +48,7 @@ namespace Cpnucleo.Infra.CrossCutting.IoC
                 .AddScoped<ICrudRepository<Sistema>, CrudRepository<Sistema>>()
                 .AddScoped<ICrudRepository<Projeto>, CrudRepository<Projeto>>()
                 .AddScoped<ICrudRepository<Tarefa>, CrudRepository<Tarefa>>()
-                .AddScoped<ICrudRepository<Apontamento>, CrudRepository<Apontamento>>()
-                .AddScoped<ICrudRepository<Workflow>, CrudRepository<Workflow>>()
-                .AddScoped<ICrudRepository<Recurso>, CrudRepository<Recurso>>()
                 .AddScoped<ICrudRepository<Impedimento>, CrudRepository<Impedimento>>()
-                .AddScoped<ICrudRepository<ImpedimentoTarefa>, CrudRepository<ImpedimentoTarefa>>()
-                .AddScoped<ICrudRepository<RecursoProjeto>, CrudRepository<RecursoProjeto>>()
-                .AddScoped<ICrudRepository<RecursoTarefa>, CrudRepository<RecursoTarefa>>()
                 .AddScoped<ICrudRepository<TipoTarefa>, CrudRepository<TipoTarefa>>();
 
             services
