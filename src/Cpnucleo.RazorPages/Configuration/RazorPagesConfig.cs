95e0d70d017364e39339f4edea8e93f7e10a646b
Jonathan Peris <jperis.silva@gmail.com>
2021-02-03 19:20:54 -0300

    [FIX] Restauração das injeções de dependencia

---
+++ src/Cpnucleo.RazorPages/Configuration/RazorPagesConfig.cs
@@ -0,0 +1,28 @@
+﻿using Cpnucleo.RazorPages.Services;
+using Cpnucleo.RazorPages.Services.Interfaces;
+using Cpnucleo.RazorPages.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cpnucleo.RazorPages.Configuration
+{
+    public static class RazorPagesConfig
+    {
+        public static void AddRazorPagesConfigSetup(this IServiceCollection services)
+        {
+            services
+                .AddScoped<ICrudService<SistemaViewModel>, SistemaService>()
+                .AddScoped<ICrudService<ProjetoViewModel>, ProjetoService>()
+                .AddScoped<ICrudService<ImpedimentoViewModel>, ImpedimentoService>()
+                .AddScoped<ICrudService<TipoTarefaViewModel>, TipoTarefaService>()
+                .AddScoped<ICrudService<WorkflowViewModel>, WorkflowService>();
+
+            services
+                .AddScoped<ITarefaService, TarefaService>()
+                .AddScoped<IApontamentoService, ApontamentoService>()
+                .AddScoped<IRecursoService, RecursoService>()
+                .AddScoped<IImpedimentoTarefaService, ImpedimentoTarefaService>()
+                .AddScoped<IRecursoProjetoService, RecursoProjetoService>()
+                .AddScoped<IRecursoTarefaService, RecursoTarefaService>();
+        }
+    }
+}
--- src/Cpnucleo.RazorPages/Startup.cs
+++ src/Cpnucleo.RazorPages/Startup.cs
@@ -27,6 +27,7 @@ namespace Cpnucleo.RazorPages
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPagesConfigSetup();
             services.AddCpnucleoSetup();
 
             services.AddAntiforgery(options => options.HeaderName = "XSRF-TOKEN");
