5312ac2d41d01de1a3e2221d0a7a5a6dfd2c9f77
Jonathan Peris <jperis.silva@gmail.com>
2021-02-04 00:43:16 -0300

    [CHORE] Unificação dos objetos de autenticação

--- src/Cpnucleo.RazorPages/Pages/Login.cshtml.cs
+++ src/Cpnucleo.RazorPages/Pages/Login.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Cpnucleo.RazorPages.Services.Interfaces;
 using Cpnucleo.Infra.CrossCutting.Util.Interfaces;
 using Cpnucleo.RazorPages.ViewModels;
-using Cpnucleo.RazorPages.Models;
 using Cpnucleo.RazorPages.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
@@ -25,7 +24,7 @@ namespace Cpnucleo.RazorPages.Pages
         }
 
         [BindProperty]
-        public LoginViewModel Login { get; set; }
+        public RecursoViewModel Recurso { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string returnUrl = null, bool logout = false)
         {
@@ -58,9 +57,9 @@ namespace Cpnucleo.RazorPages.Pages
                     return Page();
                 }
 
-                RecursoViewModel recurso = await _recursoService.AutenticarAsync(Login.Usuario, Login.Senha);
+                Recurso = await _recursoService.AutenticarAsync(Recurso.Login, Recurso.Senha);
 
-                if (recurso.Id == Guid.Empty)
+                if (Recurso.Id == Guid.Empty)
                 {
                     ModelState.AddModelError(string.Empty, "Usuário ou senha inválidos.");
                     return Page();
@@ -68,8 +67,8 @@ namespace Cpnucleo.RazorPages.Pages
 
                 IEnumerable<Claim> claims = new[]
                 {
-                    new Claim(ClaimTypes.PrimarySid, recurso.Id.ToString()),
-                    new Claim(ClaimTypes.Hash, recurso.Token)
+                    new Claim(ClaimTypes.PrimarySid, Recurso.Id.ToString()),
+                    new Claim(ClaimTypes.Hash, Recurso.Token)
                 };
 
                 ClaimsPrincipal principal = ClaimsService.CreateClaimsPrincipal(claims);
