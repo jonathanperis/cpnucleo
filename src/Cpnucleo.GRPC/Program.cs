4f4622da4a3b6ef926ef6aa93a15633592004a24
Jonathan Peris <jperis.silva@gmail.com>
2021-10-01 00:19:18 -0300

    Atualizado o Program para o padrão do .NET 6

--- src/Cpnucleo.GRPC/Program.cs
+++ src/Cpnucleo.GRPC/Program.cs
@@ -1,20 +1,72 @@
-namespace Cpnucleo.GRPC;
+using Cpnucleo.Infra.CrossCutting.IoC;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+builder.Services.AddCpnucleoSetup();
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddMagicOnion();
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
-    // Additional configuration is required to successfully run gRPC on macOS.
-    // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-    public static IHostBuilder CreateHostBuilder(string[] args)
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
     {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
-}
+        policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireClaim(ClaimTypes.PrimarySid);
+        policy.RequireClaim(ClaimTypes.Hash);
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"])),
+        };
+    });
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+app.UseCors();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapMagicOnionService().RequireCors("AllowAll");
+
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    });
+});
+
+app.Run();
--- src/Cpnucleo.MVC/Program.cs
+++ src/Cpnucleo.MVC/Program.cs
@@ -1,18 +1,80 @@
-namespace Cpnucleo.MVC;
+using Cpnucleo.MVC.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
+using StackExchange.Profiling.Storage;
+using System.Globalization;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvcConfigSetup();
+
+builder.Services.AddAntiforgery(options => options.HeaderName = "XSRF-TOKEN");
+
+builder.Services.Configure<ApplicationConfigurations>(
+    builder.Configuration.GetSection("ApplicationConfigurations"));
+
+builder.Services
+    .AddAuthentication(x =>
+    {
+        x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        x.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(x =>
+    {
+        x.LoginPath = new PathString("/Home/Login");
+        x.AccessDeniedPath = new PathString("/Home/Negado");
+    });
+
+builder.Services.AddMiniProfiler(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    // All of this is optional. You can simply call .AddMiniProfiler() for all defaults
 
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
+    // (Optional) Path to use for profiler URLs, default is /mini-profiler-resources
+    options.RouteBasePath = "/profiler";
+
+    // (Optional) Control storage
+    // (default is 30 minutes in MemoryCacheStorage)
+    (options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(60);
+
+    // (Optional) You can disable "Connection Open()", "Connection Close()" (and async variant) tracking.
+    // (defaults to true, and connection opening/closing is tracked)
+    options.TrackConnectionOpenClose = true;
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Erro");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions
+{
+    SupportedCultures = new List<CultureInfo> { new CultureInfo("pt-BR") },
+    SupportedUICultures = new List<CultureInfo> { new CultureInfo("pt-BR") },
+    DefaultRequestCulture = new RequestCulture("pt-BR")
+};
+
+app.UseMiniProfiler();
+
+app.UseRequestLocalization(localizationOptions);
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Login}/{id?}");
+
+app.Run();
