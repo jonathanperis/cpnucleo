d5c9cc9363ac20c06877f69d9030585c49ff2acb
Jonathan Peris <jperis.silva@gmail.com>
2021-10-28 14:09:41 -0300

    Implementação de regra de negocio de testes no event handler

--- src/Cpnucleo.Application/Events/Sistema/RemoveSistemaEventHandler.cs
+++ src/Cpnucleo.Application/Events/Sistema/RemoveSistemaEventHandler.cs
@@ -5,8 +5,18 @@ namespace Cpnucleo.Application.Events.Sistema;
 
 public class RemoveSistemaEventHandler : IMessageReceptionHandler<RemoveSistemaEvent>
 {
-    public Task Handle(RemoveSistemaEvent @event, CancellationToken cancellationToken)
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public RemoveSistemaEventHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        return Task.CompletedTask;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task Handle(RemoveSistemaEvent @event, CancellationToken cancellationToken)
+    {
+        //Some business logic here.
+        IEnumerable<SistemaViewModel> sistemas = _mapper.Map<IEnumerable<SistemaViewModel>>(await _unitOfWork.SistemaRepository.AllAsync(true));
     }
 }
