5ad278211237f0a7075eb73645fcac123f99ef51
Jonathan Peris <jperis.silva@gmail.com>
2021-10-29 15:59:31 -0300

    Mock do SignalR no projeto de testes

---
+++ test/Cpnucleo.Application.Test/Helpers/ServiceBusHelper.cs
@@ -0,0 +1,15 @@
+﻿using Cpnucleo.Infra.CrossCutting.Bus.Interfaces;
+using Moq;
+
+namespace Cpnucleo.Application.Test.Helpers;
+
+public class ServiceBusHelper
+{
+    public static Mock<IEventHandler> GetInstance(object @object = default)
+    {
+        Mock<IEventHandler> mockServiceBus = new();
+        mockServiceBus.Setup(x => x.PublishEventAsync(@object)).Returns(Task.CompletedTask);
+
+        return mockServiceBus;
+    }
+}
---
+++ test/Cpnucleo.Application.Test/Helpers/SignalRHelper.cs
@@ -0,0 +1,21 @@
+﻿using Cpnucleo.Application.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+
+namespace Cpnucleo.Application.Test.Helpers;
+
+public class SignalRHelper
+{
+    public static Mock<IHubContext<CpnucleoHub>> GetInstance()
+    {
+        Mock<IClientProxy> mockClientProxy = new();
+
+        Mock<IHubClients> mockClients = new();
+        mockClients.Setup(clients => clients.All).Returns(mockClientProxy.Object);
+
+        Mock<IHubContext<CpnucleoHub>> mockHub = new();
+        mockHub.Setup(x => x.Clients).Returns(() => mockClients.Object);
+
+        return mockHub;
+    }
+}
