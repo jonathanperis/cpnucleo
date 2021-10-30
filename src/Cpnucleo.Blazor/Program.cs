c27bb1497d346748f2863031a2d6b3279a836ca0
Jonathan Peris <jperis.silva@gmail.com>
2021-10-29 21:49:00 -0300

    Ajustes no projeto blazor

--- src/Cpnucleo.Blazor/Program.cs
+++ src/Cpnucleo.Blazor/Program.cs
@@ -1,21 +1,21 @@
+using Cpnucleo.Blazor;
 using Cpnucleo.Blazor.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Refit;
 
-namespace Cpnucleo.Blazor
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services
-                .AddRefitClient<ICpnucleoApiService>()
-                .ConfigureHttpClient(c => c.BaseAddress = new Uri($"{builder.Configuration.GetValue<string>("AppSettings:UrlCpnucleoApi")}/api/v2"));
+//builder.Services.AddScoped(sp => 
+//    new HttpClient 
+//    { 
+//        BaseAddress = new Uri($"{builder.Configuration.GetValue<string>("AppSettings:UrlCpnucleoApi")}/api/v2")
+//    });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services
+    .AddRefitClient<ICpnucleoApiService>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri($"{builder.Configuration.GetValue<string>("AppSettings:UrlCpnucleoApi")}/api/v2"));
+
+await builder.Build().RunAsync();
