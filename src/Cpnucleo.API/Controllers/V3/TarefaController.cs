af485c7a5e4c54d05c9860332ebbd2c54514ec6e
Jonathan Peris <jperis.silva@gmail.com>
2022-02-06 13:05:27 -0300

    Adicionada outras controllers restantes

---
+++ src/Cpnucleo.API/Controllers/V3/TarefaController.cs
@@ -0,0 +1,137 @@
+﻿using Cpnucleo.Application;
+using Cpnucleo.Application.Commands.Tarefa.CreateTarefa;
+using Cpnucleo.Application.Commands.Tarefa.RemoveTarefa;
+using Cpnucleo.Application.Commands.Tarefa.UpdateByWorkflow;
+using Cpnucleo.Application.Commands.Tarefa.UpdateTarefa;
+using Cpnucleo.Application.Queries.Tarefa.GetByRecurso;
+using Cpnucleo.Application.Queries.Tarefa.GetTarefa;
+using Cpnucleo.Application.Queries.Tarefa.ListTarefa;
+
+namespace Cpnucleo.API.Controllers.V3;
+
+//[Authorize]
+[ApiController]
+[ApiVersion("3")]
+[Produces("application/json")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class TarefaController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TarefaController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Listar tarefas
+    /// </summary>
+    /// <remarks>
+    /// # Listar tarefas
+    /// 
+    /// Lista tarefas da base de dados.
+    /// </remarks>
+    /// <param name="query">Objeto de consulta com os parametros necessários</param>        
+    [HttpGet]
+    [Route("ListTarefa")]
+    public async Task<ActionResult<ListTarefaViewModel>> ListTarefa([FromQuery] ListTarefaQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+
+    /// <summary>
+    /// Consultar tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Consultar tarefa
+    /// 
+    /// Consulta um tarefa na base de dados.
+    /// </remarks>
+    /// <param name="query">Objeto de consulta com os parametros necessários</param>        
+    [HttpGet]
+    [Route("GetTarefa")]
+    public async Task<ActionResult<GetTarefaViewModel>> GetTarefa([FromQuery] GetTarefaQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+
+    /// <summary>
+    /// Consultar tarefa por recurso
+    /// </summary>
+    /// <remarks>
+    /// # Consultar tarefa por recurso
+    /// 
+    /// Consulta um tarefa por recurso na base de dados.
+    /// </remarks>
+    /// <param name="query">Objeto de consulta com os parametros necessários</param>        
+    [HttpGet]
+    [Route("GetByRecurso")]
+    public async Task<ActionResult<GetByRecursoViewModel>> GetByRecurso([FromQuery] GetByRecursoQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+
+    /// <summary>
+    /// Incluir tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Incluir tarefa
+    /// 
+    /// Inclui um tarefa na base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpPost]
+    [Route("PostTarefa")]
+    public async Task<ActionResult<OperationResult>> PostTarefa([FromBody] CreateTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    /// <summary>
+    /// Alterar tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Alterar tarefa
+    /// 
+    /// Altera um tarefa na base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpPut]
+    [Route("PutTarefa")]
+    public async Task<ActionResult<OperationResult>> PutTarefa([FromBody] UpdateTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    /// <summary>
+    /// Alterar workflow da tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Alterar workflow da tarefa
+    /// 
+    /// Altera um workflow da tarefa na base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpPut]
+    [Route("PutByWorkflow")]
+    public async Task<ActionResult<OperationResult>> PutByWorkflowTarefa([FromBody] UpdateByWorkflowCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    /// <summary>
+    /// Remover tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Remover tarefa
+    /// 
+    /// Remove um tarefa da base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpDelete]
+    [Route("DeleteTarefa")]
+    public async Task<ActionResult<OperationResult>> DeleteTarefa([FromBody] RemoveTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
---
+++ src/Cpnucleo.API/Controllers/V3/TipoTarefaController.cs
@@ -0,0 +1,103 @@
+﻿using Cpnucleo.Application;
+using Cpnucleo.Application.Commands.TipoTarefa.CreateTipoTarefa;
+using Cpnucleo.Application.Commands.TipoTarefa.RemoveTipoTarefa;
+using Cpnucleo.Application.Commands.TipoTarefa.UpdateTipoTarefa;
+using Cpnucleo.Application.Queries.TipoTarefa.GetTipoTarefa;
+using Cpnucleo.Application.Queries.TipoTarefa.ListTipoTarefa;
+
+namespace Cpnucleo.API.Controllers.V3;
+
+//[Authorize]
+[ApiController]
+[ApiVersion("3")]
+[Produces("application/json")]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class TipoTarefaController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TipoTarefaController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Listar tipos de tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Listar tipos de tarefa
+    /// 
+    /// Lista tipos de tarefa da base de dados.
+    /// </remarks>
+    /// <param name="query">Objeto de consulta com os parametros necessários</param>        
+    [HttpGet]
+    [Route("ListTipoTarefa")]
+    public async Task<ActionResult<ListTipoTarefaViewModel>> ListTipoTarefa([FromQuery] ListTipoTarefaQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+
+    /// <summary>
+    /// Consultar tipo de tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Consultar tipo de tarefa
+    /// 
+    /// Consulta um tipo de tarefa na base de dados.
+    /// </remarks>
+    /// <param name="query">Objeto de consulta com os parametros necessários</param>        
+    [HttpGet]
+    [Route("GetTipoTarefa")]
+    public async Task<ActionResult<GetTipoTarefaViewModel>> GetTipoTarefa([FromQuery] GetTipoTarefaQuery query)
+    {
+        return await _mediator.Send(query);
+    }
+
+    /// <summary>
+    /// Incluir tipo de tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Incluir tipo de tarefa
+    /// 
+    /// Inclui um tipo de tarefa na base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpPost]
+    [Route("PostTipoTarefa")]
+    public async Task<ActionResult<OperationResult>> PostTipoTarefa([FromBody] CreateTipoTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    /// <summary>
+    /// Alterar tipo de tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Alterar tipo de tarefa
+    /// 
+    /// Altera um tipo de tarefa na base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpPut]
+    [Route("PutTipoTarefa")]
+    public async Task<ActionResult<OperationResult>> PutTipoTarefa([FromBody] UpdateTipoTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    /// <summary>
+    /// Remover tipo de tarefa
+    /// </summary>
+    /// <remarks>
+    /// # Remover tipo de tarefa
+    /// 
+    /// Remove um tipo de tarefa da base de dados.
+    /// </remarks>
+    /// <param name="command">Objeto de envio com os parametros necessários</param>        
+    [HttpDelete]
+    [Route("DeleteTipoTarefa")]
+    public async Task<ActionResult<OperationResult>> DeleteTipoTarefa([FromBody] RemoveTipoTarefaCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
