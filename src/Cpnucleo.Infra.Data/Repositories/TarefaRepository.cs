9e588fbcff7de05e522725a6fbfa695c4a941dd8
Jonathan Peris <jperis.silva@gmail.com>
2022-06-10 01:53:03 -0300

    Update TarefaRepository.cs

--- src/Cpnucleo.Infra.Data/Repositories/TarefaRepository.cs
+++ src/Cpnucleo.Infra.Data/Repositories/TarefaRepository.cs
@@ -8,10 +8,10 @@ internal class TarefaRepository : GenericRepository<Tarefa>, ITarefaRepository
     public async Task<IEnumerable<Tarefa>> GetTarefaByRecursoAsync(Guid idRecurso)
     {
         return await _context.Set<RecursoTarefa>()
-                   .AsQueryable()
-                   .Include(_context.GetIncludePaths(typeof(RecursoTarefa)))
-                   .Where(x => x.IdRecurso == idRecurso && x.Ativo)
-                   .Select(x => x.Tarefa)
-                   .ToListAsync();
+            .AsQueryable()
+            .Include(_context.GetIncludePaths(typeof(RecursoTarefa)))
+            .Where(x => x.IdRecurso == idRecurso && x.Ativo)
+            .Select(x => x.Tarefa)
+            .ToListAsync();
     }
 }
