8f495ae682a5d58eff676031e4b15bb92499dfef
Jonathan Peris <jperis.silva@gmail.com>
2022-06-11 17:02:19 -0300

    [feat] Mais configurações necessárias para migrações automaticas

---
+++ src/Cpnucleo.Infra.Data/MigrationManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cpnucleo.Infra.Data;
+
+public static class MigrationManager
+{
+    public static WebApplication MigrateDatabase(this WebApplication webApp)
+    {
+        using (var scope = webApp.Services.CreateScope())
+        {
+            using (var appContext = scope.ServiceProvider.GetRequiredService<CpnucleoContext>())
+            {
+                try
+                {
+                    appContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    //Log errors or do anything you think it's needed
+                    throw;
+                }
+            }
+        }
+        return webApp;
+    }
+}
