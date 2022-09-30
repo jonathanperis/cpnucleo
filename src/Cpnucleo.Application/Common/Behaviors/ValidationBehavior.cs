ecd7211b4754bb5e45307e0bd702f31752dac0a9
Jonathan Peris <jperis.silva@gmail.com>
2022-09-30 12:53:52 -0300

    [feat] Updated Mediatr and AutoMapper

--- src/Cpnucleo.Application/Common/Behaviors/ValidationBehavior.cs
+++ src/Cpnucleo.Application/Common/Behaviors/ValidationBehavior.cs
@@ -12,7 +12,7 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
         _validators = validators;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (_validators.Any())
         {
