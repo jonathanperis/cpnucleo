9c197dde860797ddf945a6c83cbaba436fb63611
Jonathan Peris <jperis.silva@gmail.com>
2022-09-30 12:53:52 -0300

    [feat] Updated Mediatr and AutoMapper

--- src/Cpnucleo.Application/Common/Behaviors/UnhandledExceptionBehavior.cs
+++ src/Cpnucleo.Application/Common/Behaviors/UnhandledExceptionBehavior.cs
@@ -11,7 +11,7 @@ public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior
         _logger = logger;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         try
         {
