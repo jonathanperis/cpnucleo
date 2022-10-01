f8708a6750b74f5a2ca1015f86cceee5463a8f27
Jonathan Peris <jperis.silva@gmail.com>
2022-10-01 09:38:08 -0300

    Update Program.cs

--- src/Cpnucleo.GRPC/Program.cs
+++ src/Cpnucleo.GRPC/Program.cs
@@ -82,4 +82,6 @@ app.UseEndpoints(endpoints =>
     endpoints.MapMagicOnionService().RequireCors("AllowAll");
 });
 
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 app.Run();
