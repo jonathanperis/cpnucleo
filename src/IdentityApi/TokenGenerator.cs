084b974f2423186c31057602ad253389f4863862
Jonathan Peris <jperis.silva@gmail.com>
2025-02-17 21:10:03 -0300

    Chore: Code cleanup

--- src/IdentityApi/TokenGenerator.cs
+++ src/IdentityApi/TokenGenerator.cs
@@ -14,13 +14,15 @@ public class TokenGenerator
             new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new(JwtRegisteredClaimNames.Email, email),
         };
-        
-        var tokenDescriptor = new SecurityTokenDescriptor();
-        tokenDescriptor.Subject = new ClaimsIdentity(claims);
-        tokenDescriptor.Expires = DateTime.UtcNow.AddHours(1);
-        tokenDescriptor.Issuer = "https://identity.peris-studio.dev";
-        tokenDescriptor.Audience = "https://peris-studio.dev";
-        tokenDescriptor.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(1),
+            Issuer = "https://identity.peris-studio.dev",
+            Audience = "https://peris-studio.dev",
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
 
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
