e407729ae49c67552b4c20e33a21dc4a24d3102b
Jonathan Peris <jperis.silva@gmail.com>
2025-03-23 17:37:36 -0300

    Feat: Removed Grpc innitial project

--- src/GrpcServer/Usings.cs
+++
@@ -1,7 +0,0 @@
-global using GrpcServer;
-global using Microsoft.AspNetCore.Identity.Data;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.IdentityModel.Tokens;
-global using Microsoft.OpenApi.Models;
-global using System.IdentityModel.Tokens.Jwt;
-global using System.Security.Claims;
