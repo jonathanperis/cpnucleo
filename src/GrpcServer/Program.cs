b53b8d85dcae25e86d85cdf5f37b3423bf089502
Jonathan Peris <jperis.silva@gmail.com>
2025-03-23 21:23:58 -0300

    Feat: GrpcService Skeleton

---
+++ src/GrpcServer/Program.cs
@@ -0,0 +1,51 @@
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.ConfigureOpenTelemetry();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services
+    .AddApiVersioning(options =>
+    {
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+    })
+    .AddApiExplorer(
+        options =>
+        {
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+        })
+    .EnableApiVersionBinding();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.AddGrpc();
+builder.Services.AddMagicOnion();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+
+// app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+// app.UseCors("AllowAll");
+
+app.UseInfrastructure();
+
+app.UseHttpsRedirection();
+app.UseHsts();
+
+app.MapMagicOnionService();
+
+app.MapHealthChecks("/healthz");
+app.MapGet("/", () => "Cpnucleo GRPC it's up and ready to receive requests.");
+
+app.Run();
---
+++ src/GrpcServer/Usings.cs
@@ -0,0 +1,4 @@
+global using Application;
+global using Asp.Versioning;
+global using GrpcServer.Configurations;
+global using Infrastructure;
