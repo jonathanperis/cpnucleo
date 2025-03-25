fd62308846f402cce06f88a1d289e545372f044e
Jonathan Peris <jperis.silva@gmail.com>
2025-03-25 10:09:58 -0300

    Feat: Innitial Radzen implementation for studies

---
+++ src/WebClient/Program.cs
@@ -0,0 +1,29 @@
+using Radzen;
+using WebClient.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
+
+app.Run();
