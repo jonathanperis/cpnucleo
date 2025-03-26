3aab043510eaa7c15e8fdb53b56241f772b88023
Jonathan Peris <jperis.silva@gmail.com>
2025-03-26 12:21:47 -0300

    Feat: WebClient further upgrades

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -25,9 +25,9 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapStaticAssets();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
