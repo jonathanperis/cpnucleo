bb40a441b90922f79c1083bb7431b82c144fb18b
Jonathan Peris <jperis.silva@gmail.com>
2026-05-23 22:22:22 +0000

    chore: remove Blazor WebClient shell

--- src/WebClient/Components/Organizations/OrganizationViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebClient.Components.Organizations;
-
-public class OrganizationViewModel
-{
-    public Guid Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
